d6865d138b1c9a21383f55dad25df484004ddfac
Jessica Stayton <jessica.stayton@gmail.com>
2021-04-08 20:30:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random() * 100) + 1);
+  var itemObj = {itemName: item, itemPrice: price};
+    cart.push(itemObj);
+  return `${item} has been added to your cart.`;
 }
 
+
 function viewCart() {
-  // write your code here
+      if (cart.length === 0) {
+        return `Your shopping cart is empty.`
+      } else if (cart.length === 1) {
+        return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+      } else if (cart.length === 2) {
+        return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+      } else if (cart.length >= 3 ) {
+        return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
+      }
 }
 
 function total() {
-  // write your code here
+  let allPrices = []
+  let sum = 0
+  for (var i = 0; i < cart.length; i++) {
+    allPrices.push(cart[i].itemPrice)
+    sum += allPrices[i]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
